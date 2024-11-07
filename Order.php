3c5e7774cc814c3645ce8e0446469447302ed813
TatianaAll <tatiana.allery@orange.fr>
2024-11-07 12:37:28 +0100

    add the magic method construct

--- Order.php
+++ Order.php
@@ -4,15 +4,14 @@ class Order
 {
     public $id;
     public $customerName;
-//    j'initialise mon status de base en "cart"
     public $status = "cart";
     public $totalPrice;
-//    j'initialise la variable products en tableau vide
     public $products = [];
-// je créée une fonction addProduct qui ajoute un produit
-//cette fonction ajoute un produit à mon tableau de produit
-//elle ajoute aussi le prix du produit au prix total
-//elle ne se réalise que si la commande est en cours
+// adding the magic method __construct to get the custumerName during the creatin of the new instance of the Order class
+    public function __construct($customerName){
+        $this->customerName = $customerName;
+    }
+
     public function addProduct()
     {
         if ($this->status === "cart") {
@@ -21,39 +20,35 @@ class Order
         }
     }
 
-//pour payer je vérifie la condition de mon status qui va passer de "cart" à "payed"
     public function pay()
     {
         if ($this->status === "cart") {
             $this->status = "payed";
         }
     }
-//    je vérifie que ma commande n'a pas été payé ou validé
-//je vérifie que le panier n'est pas vide
-//si ces conditions sont remplies je retire le dernier item ajouté avec la fonction native array_pop()
+
+//remove the last item of the array with the native function array_pop()
     public function removeProduct(){
         if ($this->status === "cart" && count($this->products)!==0) {
             array_pop($this->products);
+            $this->totalPrice-=3;
         } else if (count($this->products)===0){
             echo ("votre panier est vide");
         }
     }
 }
-
-//je fais ma premiere commande :
-$order1 = new Order();
-//j'ajoute mon produit :
-$order1->addProduct();
-$order1->addProduct();
-$order1->addProduct();
-var_dump($order1);
-$order1->pay();
-
-$order1->addProduct();
-$order1->removeProduct();
-var_dump($order1);
-
-$order2 = new Order();
-$order2->addProduct();
-$order2->removeProduct();
-var_dump($order2);
+// creation of the first instance
+$newOrder1 = new Order("Tatiana");
+$newOrder1->addProduct();
+$newOrder1->addProduct();
+var_dump($newOrder1); ?>
+<br>
+<?php
+$newOrder1->removeProduct();
+var_dump($newOrder1); ?>
+    <br>
+<?php
+$newOrder1->pay();
+var_dump($newOrder1); ?>
+    <br>
+<?php
