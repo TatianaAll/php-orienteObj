cb566e0f0a50e7f561e879afb6784952bb1a234f
TatianaAll <tatiana.allery@orange.fr>
2024-11-08 14:48:51 +0100

    inheritance

---
+++ DigitalProduct.php
@@ -0,0 +1,8 @@
+<?php
+require_once ('./Product.php');
+class DigitalProduct extends Product {
+
+}
+
+$digital1 = new DigitalProduct();
+var_dump($digital1);
---
+++ Product.php
@@ -0,0 +1,18 @@
+<?php
+
+class Product{
+    protected $id;
+    protected $price;
+    protected $title;
+    protected $shippingCost;
+
+    protected function buy()
+    {
+        //buy the product
+    }
+    protected function ship()
+    {
+        //shipping the product
+    }
+
+}
